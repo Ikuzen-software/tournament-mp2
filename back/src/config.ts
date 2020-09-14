452d84f3d5edf72c7d02f3bcc9c1e4e26c0f9b0c
Ikuzen <yannis.samba@gmail.com>
2020-09-14 22:39:38 +0200

    change color of the theme, update the UML diagram

--- back/src/config.ts
+++ back/src/config.ts
@@ -9,17 +9,17 @@ function env(name: string): string {
     const value = process.env[name]?.trim();
 
     if (!value) {
-        throw new Error(`Environment variable "${ name }" must be defined`);
+        throw new Error(`Environment variable "${name}" must be defined`);
     }
 
     return value;
 }
 
 export let uri: string;
-    const username = env('TOURNAMENT_MONGO_USERNAME');
-    const password = env('TOURNAMENT_MONGO_PASSWORD');
-    const database = env('TOURNAMENT_MONGO_DATABASE');
-    const protocol = env('TOURNAMENT_MONGO_PROTOCOL');
-    const host = env('TOURNAMENT_MONGO_HOST');
-    uri = `${protocol }+srv://${ username }:${ password }@${ host }/${ database }`;
-export const allowedOrigins = [ 'http://localhost:3000', 'http://localhost:4200' ];
+const username = env('TOURNAMENT_MONGO_USERNAME');
+const password = env('TOURNAMENT_MONGO_PASSWORD');
+const database = env('TOURNAMENT_MONGO_DATABASE');
+const protocol = env('TOURNAMENT_MONGO_PROTOCOL');
+const host = env('TOURNAMENT_MONGO_HOST');
+uri = `${protocol}+srv://${username}:${password}@${host}/${database}`;
+export const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200'];
